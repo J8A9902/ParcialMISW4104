58d3301ff73d79135703825d35f6859d21eb1a63
J8A9902 <j.ochoao@uniandes.edu.co>
2022-05-01 21:45:03 -0500

    Se agrega prueba unitaria al servicio listas

--- src/app/cafe/cafe.service.spec.ts
+++ src/app/cafe/cafe.service.spec.ts
@@ -2,15 +2,56 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CafeService } from './cafe.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { faker } from '@faker-js/faker';
+import { Cafe } from './cafe'
+
+
+let httpTestingController: HttpTestingController;
+let service: CafeService;
+let Cafes: Array<Cafe> = [];
 
 describe('Service: Cafe', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CafeService]
+      imports: [HttpClientTestingModule],
+      providers: [CafeService],
+    });
+
+    Cafes = [];
+
+    for (let i = 0; i < 3; i++) {
+      Cafes.push(
+        new Cafe(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.lorem.sentence()
+        )
+      );
+    }
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(CafeService);
+  });
+
+  it('should create ok', inject(
+    [CafeService],
+    (service: CafeService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+
+  it('if consume getCafes returned Observable should match the right data', () => {
+    service.getCafes().subscribe((Cafes) => {
+      expect(Cafes.length).toEqual(3);
     });
   });
 
-  it('should ...', inject([CafeService], (service: CafeService) => {
-    expect(service).toBeTruthy();
-  }));
+
 });
